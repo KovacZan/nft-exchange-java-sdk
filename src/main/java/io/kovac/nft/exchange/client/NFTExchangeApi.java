36aa04df7e7160e310e153aee838060f30616011
KovacZan <zan@protokol.com>
2020-12-25 17:41:40 +0100

    Implement NFTExchangeApi

--- src/main/java/io/kovac/nft/exchange/client/NFTExchangeApi.java
+++ src/main/java/io/kovac/nft/exchange/client/NFTExchangeApi.java
@@ -1,11 +1,27 @@
 package io.kovac.nft.exchange.client;
 
+import io.kovac.nft.exchange.client.implementations.Auctions;
+import io.kovac.nft.exchange.client.implementations.Bids;
+import io.kovac.nft.exchange.client.implementations.Configurations;
+import io.kovac.nft.exchange.client.implementations.Trades;
+import io.kovac.nft.exchange.client.interfaces.IAuctions;
+import io.kovac.nft.exchange.client.interfaces.IBids;
+import io.kovac.nft.exchange.client.interfaces.IConfigurations;
+import io.kovac.nft.exchange.client.interfaces.ITrades;
 import org.arkecosystem.client.http.Client;
 
 public class NFTExchangeApi {
 
-    public NFTExchangeApi(Client client) {
+    public final IAuctions auctions;
+    public final IBids bids;
+    public final IConfigurations configurations;
+    public final ITrades trades;
 
+    public NFTExchangeApi(Client client) {
+        this.auctions = new Auctions(client);
+        this.bids = new Bids(client);
+        this.configurations = new Configurations(client);
+        this.trades = new Trades(client);
     }
 
 }
